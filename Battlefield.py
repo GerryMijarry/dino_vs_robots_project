a2b4b8e1b5bdc00b0b1e548d7f205c4228b9a5f9
Gerard Mijares <gerardmijares39@gmail.com>
2021-09-11 06:13:02 +0400

    finished game logic and completed challenge

--- Battlefield.py
+++ Battlefield.py
@@ -1,5 +1,6 @@
 from fleet import Fleet
 from herd import Herd
+import random
 
 class Battlefield:
     def __init__(self):
@@ -15,19 +16,52 @@ class Battlefield:
         print("Welcome to Dino Vs. Robots!!")
         
     def battle(self):
-        pass
+        while len(self.herd.dinosaurs) > 0 and len(self.fleet.robots) > 0:
+            dice = random.randint(1, 10)
+            if dice > 5:
+                self.dino_turn()
+                if len(self.fleet.robots) > 0:
+                    self.robo_turn()
+            else:
+                self.robo_turn()
+                if len(self.herd.dinosaurs) > 0:
+                    self.dino_turn()
         
     def dino_turn(self):
-        pass
+        self.show_dino_opponent_options()
+        dino_attacker = int(input('Which dino will be the attacker?'))
+        self.show_robot_opponent_options()
+        robot_target = int(input('Which robot will be the target?'))
+        self.herd.dinosaurs[dino_attacker].attack(
+            self.fleet.robots[robot_target])
+        if self.fleet.robots[robot_target].health <= 0:
+            print(str(self.fleet.robots[robot_target].name) + ' has beeped his last beep!')
+            self.fleet.robots.remove(self.fleet.robots[robot_target])
 
     def robo_turn(self):
-        pass
+        self.show_robot_opponent_options()
+        robot_attacker = int(input('Which robot will be the attacker?'))
+        self.show_dino_opponent_options()
+        dino_target = int(input('Which dino will be the target?'))
+        self.fleet.robots[robot_attacker].attack(
+            self.herd.dinosaurs[dino_target])
+        if self.herd.dinosaurs[dino_target].health <= 0:
+            print(str(self.herd.dinosaurs[dino_target].name) + ' has rawr\'d his last rawr!')
+            self.herd.dinosaurs.remove(self.herd.dinosaurs[dino_target])
 
     def show_dino_opponent_options(self):
-        pass
+        print("Select your dino!")
+        counter = 0
+        for dino in self.herd.dinosaurs:
+            print('Press ' + str(counter) + ' to select ' + dino.name)
+            counter +=1
 
     def show_robot_opponent_options(self):
-        pass
+        print("Select your robot!")
+        counter = 0
+        for robot in self.fleet.robots:
+            print('Press ' + str(counter) + ' to select ' + robot.name)
+            counter +=1
 
     def display_winners(self):
         if len(self.fleet.robots) > len(self.herd.dinosaurs):
