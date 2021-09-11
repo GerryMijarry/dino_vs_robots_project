1cae3823441b798320e4db961109545cddee32cc
Gerard Mijares <gerardmijares39@gmail.com>
2021-09-11 04:10:41 +0400

    refactored all files tostart with lowercase and scaffolded battlefield.py

---
+++ Battlefield.py
@@ -0,0 +1,27 @@
+class Battlefield:
+    def __init__(self):
+        pass
+
+    def run_game(self):
+        self.display_welcome
+
+    def display_welcome(self):
+        print("Welcome to Dino Vs. Robots!!")
+        
+    def battle(self):
+        pass
+        
+    def dino_turn(self):
+        pass
+
+    def robo_turn(self):
+        pass
+
+    def show_dino_opponent_options(self):
+        pass
+
+    def show_robot_opponent_options(self):
+        pass
+
+    def display_winners(self):
+        pass
